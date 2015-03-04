f899074e388b4dd3819f855e49e6900cfb6a8542
nitya <n.narasimhan@gmail.com>
2015-03-04 13:17:56 -0500

    WebApp-3a: Using Collections

--- 2015-03-MHVLUG/webapp/webapp.js
+++ 2015-03-MHVLUG/webapp/webapp.js
@@ -1,11 +1,19 @@
 // simple-todos.js
+
+// TODO: Add tasks from console to populate collection
+// On Server:
+// db.tasks.insert({ text: "Hello world!", createdAt: new Date() });
+// On Client:
+// Tasks.insert({ text: "Hello world!", createdAt: new Date() });
+Tasks = new Mongo.Collection("tasks");
+
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 }
+
